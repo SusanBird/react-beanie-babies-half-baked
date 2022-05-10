b019c1a68a11406fc1b852f21c71de290cbd95da
SusanBird <susanbird391@gmail.com>
2022-05-09 17:46:09 -0700

    aborted attempt at the searchbar

--- src/BeaniesPage.js
+++ src/BeaniesPage.js
@@ -7,12 +7,11 @@ export default function BeaniesPage() {
   const [beanieBabies, setBeanieBabies] = useState([]);
   const [lastPage, setLastPage] = useState(100);
   const [page, setPage] = useState(1);
+  // const [searchedBeanies, setSearchedBeanies] = useState([]);
   // const perPage = 40;
   
   useEffect(() => {
     async function fetch() {
-      // const from = page * perPage - perPage;
-      // const to = page * perPage;
       const beanies = await getBeanieBabies(page);
 
       setBeanieBabies(beanies.body);
@@ -22,9 +21,29 @@ export default function BeaniesPage() {
     fetch();
   }, [page]); // what can you do with this array to trigger a fetch every time the page changes?
 
+
+  // useEffect(() => {
+  //   async function searchBeanies(userInput) {
+  //     if (userInput) {
+  //       const matchingBeanieBabies = beanieBabies
+  //         .filter(beanieBaby => beanieBaby.title.toLowerCase()
+  //           .includes(userInput.toLowerCase())
+  //         );
+  //       setSearchedBeanies([...matchingBeanieBabies]);
+  //     } else {
+  //       setSearchedBeanies([...beanieBabies]);
+  //     }
+  //   }
+  //   searchBeanies();
+  // }, [page]);
+
   return (
     <>
       <h2>Page {page}</h2>
+      {/* <div>
+        Search for Beanie Babies by Title
+        <input onChange={e => searchBeanies(e.target.value)} /> */}
+      {/* </div> */}
       <div className='buttons'>
         {/* on click, this button should decrement the page in state  */}
         {/* also, disable this button when you are on the first page */}
